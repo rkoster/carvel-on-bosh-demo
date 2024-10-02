90c937e3a9d7483c709f65ac28bbb4b3f38450d1
rkoster <hi@rkoster.dev>
2024-10-02 15:37:51 +0200

    Generate registry-data package

---
+++ manifest/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+config="$(bosh cloud-config | spruce json | jq -c .)"
+
+export BOSH_vm_type="$(echo ${config} | bosh int --path /vm_types/1/name -)"
+export BOSH_network="$(echo ${config} | bosh int --path /networks/2/name -)"
+export BOSH_azs="$(echo ${config} | bosh int --path /networks/2/subnets/0/azs -)"
+
+bosh -n deploy -d carvel-on-bosh-demo  manifest/manifest.yml --vars-env=BOSH
