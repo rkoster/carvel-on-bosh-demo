46d08f6d90097e5e1ae0aadc3a1435083fcd9907
rkoster <hi@rkoster.dev>
2024-10-03 13:38:01 +0200

    Deploy kapp-controller

--- manifest/deploy.sh
+++ manifest/deploy.sh
@@ -8,4 +8,5 @@ export BOSH_vm_type="$(echo ${config} | bosh int --path /vm_types/1/name -)"
 export BOSH_network="$(echo ${config} | bosh int --path /networks/2/name -)"
 export BOSH_azs="$(echo ${config} | bosh int --path /networks/2/subnets/0/azs -)"
 
-bosh -n deploy -d carvel-on-bosh-demo  manifest/manifest.yml --vars-env=BOSH
+bosh -n deploy -d carvel-on-bosh-demo manifest/manifest.yml --vars-env=BOSH \
+     -o manifest/kapp-controller.yml
